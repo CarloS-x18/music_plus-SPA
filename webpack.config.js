7b68073f188724dab65c73ed2fcbd384afb0d684
Carlos Alberto <carlosescobedomoreno@gmail.com>
2021-09-08 22:05:12 -0500

    Setup to project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, 'build/')
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i, // me vas a buscar todos los archivos que terminen con la extension .sass o .scss de manera global
+                use: [
+                    'style-loader', 
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body'
+        })
+    ]
+}
